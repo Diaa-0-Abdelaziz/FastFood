79a50cdb60112c9b92e413ea859d21147ec01bfd
Diaa-0-Abdelaziz <diaamohamedabelaziz@gmail.com>
2024-03-08 18:56:49 +0200

    renamed:    app/fast-food-logo.ico -> app/favicon.ico 	modified:   app/layout.js

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="./fast-food-logo.ico"/>
+        <link rel="icon" href="./favicon.ico"/>
       </head>
       <body className={inter.className} suppressHydrationWarning={true}>
         <Navbar/>
