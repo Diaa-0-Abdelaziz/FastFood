1c682ff28303aae58e514fe19d4dd1bb62fcb4d9
Diaa-0-Abdelaziz <diaamohamedabelaziz@gmail.com>
2024-03-10 08:12:43 +0200

    new file:   app/(pages)/contactUs/contact.module.css 	modified:   app/(pages)/contactUs/page.jsx 	modified:   app/components/navbar/navbar.css.map 	modified:   app/components/navbar/navbar.scss

--- app/(pages)/contactUs/page.jsx
+++ app/(pages)/contactUs/page.jsx
@@ -1,7 +1,79 @@
-import React from 'react'
-
+"use client"
+import React, { useState } from 'react'
+import styles from './contact.module.css'
 export default function ContactUs() {
-  return (
-    <div>contactUs</div>
+  const [lengthOfUserNameInputs, setLengthOfUserNameInputs] = useState(true)
+ const [lengthOfuserAgeInputs, setLengthOfuserAgeInputs] = useState(true)
+ const [lengthOfuserEmailInputs, setLengthOfuserEmailInputs] = useState(true)
+ const [lengthOfuserMessage, setLengthOfuserMessage] = useState(true)
+  
+ function showUserNameMessage(e){
+  const lengthOf = e.target.value.length
+  if(lengthOf >= 1){
+    setLengthOfUserNameInputs(false)
+  }else{
+    setLengthOfUserNameInputs(true)
+  }
+  
+}
+function showUserAgeMessage(e){
+  const lengthOf = e.target.value.length
+  if(lengthOf >= 1){
+    setLengthOfuserAgeInputs(false)
+  }else{
+    setLengthOfuserAgeInputs(true)
+  }
+  
+}
+function showUserEmailMessage(e){
+  const lengthOf = e.target.value.length
+  if(lengthOf >= 1){
+    setLengthOfuserEmailInputs(false)
+  }else{
+    setLengthOfuserEmailInputs(true)
+  }
+  
+}
+function showUserPasswordMessage(e){
+  const lengthOf = e.target.value.length
+  if(lengthOf >= 1){
+    setLengthOfuserMessage(false)
+  }else{
+    setLengthOfuserMessage(true)
+  }
+  
+}
+ return (
+  <>
+ <div className={styles.cover}>
+  <div className={styles.main}>
+     <div className="container row d-flex justify-content-center p-5">
+       <div className="col-sm-8">
+       <div className="my-5 position-relative z-3">
+         <span className={ lengthOfUserNameInputs ? `${styles.topOfInput} ${styles.changeTop}` : styles.topOfInput}>userName:</span>
+         <input type="text"onInput={showUserNameMessage} className={styles.form_inputs} placeholder='userName'/>
+       </div>
+
+       <div className="my-5 position-relative z-3">
+       <span className={ lengthOfuserAgeInputs ? `${styles.topOfInput} ${styles.changeTop}` : styles.topOfInput}>userAge:</span>
+         <input type="number" onInput={showUserAgeMessage} className={styles.form_inputs} placeholder='userAge'/>
+       </div>
+       
+       <div className="my-5 position-relative z-3">
+       <span className={ lengthOfuserEmailInputs ? `${styles.topOfInput} ${styles.changeTop}` : styles.topOfInput}>userEmail:</span>
+         <input type="email" onInput={showUserEmailMessage} className={styles.form_inputs} aria-describedby="emailHelp" placeholder='userEmail'/>
+       </div>
+
+       <div className="my-5 position-relative z-3">
+       <span className={ lengthOfuserMessage ? `${styles.topOfInput} ${styles.changeTop}` : styles.topOfInput}>Your message:</span>
+         <textarea type="text" onInput={showUserPasswordMessage} className={styles.form_inputs} placeholder='write your message'/>
+       </div>
+
+       <button type="submit" className={styles.button}>Submit</button>
+       </div>
+     </div>
+   </div>
+ </div>
+ </>
   )
 }
